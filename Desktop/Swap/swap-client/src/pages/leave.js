6f67f55bc7ed314423d07c53402df37fdfe59d1e
John Jakobsen <go4johne@gmail.com>
2020-01-17 01:34:01 -0500

    Enabled User location to sync with map displayed in Leaving Screen

--- Desktop/Swap/swap-client/src/pages/leave.js
+++ Desktop/Swap/swap-client/src/pages/leave.js
@@ -9,11 +9,32 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import 'date-fns'
 import {KeyboardTimePicker} from '@material-ui/pickers'
+import Slider from '@material-ui/core/Slider';
 
 
 ///Map imports
 import mapboxToken from '../components/maps'
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL, {Marker} from 'react-map-gl'
+
+const marks = [
+    {
+        value: 0,
+        label: 'No Flexibility'
+    },
+    {
+        value: 5,
+        label: '5 Minutes'
+    },
+    {
+        value: 10,
+        label: '10 Minutes'
+    },
+    {
+        value: 15,
+        label: '15 Minutes'
+    }
+
+]
 
 const styles = {
     title: {
@@ -29,14 +50,22 @@ const styles = {
         backgroundColor: '#f1f1f5',
         borderRadius: '15px',
         boxShadow: '0px 0px 50px 3px rgba(0,0,0, 1)',
-        width: '50%'
+        width: '50%',
+        marginBottom: '10%'
     },
     formElements: {
         width: '75%',
-        marginLeft: '15%',
-        marginRight: '15%',
-        marginBottom: '10px'
+        marginBottom: '5%'
     },
+    formButton: {
+        width: '75%',
+        marginBottom: '10px',
+        marginTop: '15%'
+    },
+    mapElement : {
+        marginBottom: '5%'
+    },
+    
 }
 
 class leave extends Component {
@@ -53,10 +82,21 @@ class leave extends Component {
             errors: {},
             loading: '',
             time: null,
-            flexibility: '',
+            flexibility: [0,15],
             userLocation: '' //Add something to get user location
 
         }
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(position => {
+            this.setState(prevState => {
+                const coords = {...prevState.viewport};
+                coords.latitude = position.coords.latitude;
+                coords.longitude = position.coords.longitude
+                return { viewport: coords }
+            })
+        })
     }
 
     handleSubmit = (event) => {
@@ -75,17 +115,26 @@ class leave extends Component {
             time
         })
     }
+
+    valueLabelFormat = value => {
+        return marks.findIndex(mark => mark.value ===value) + 1;
+    }
+
+    valueText = value => {
+        return `${value} Minutes`
+    }
    
 
     render() {
         const {classes} = this.props
         const {errors} = this.state
         return (
-            <Grid container spacing={5} direction='column' alignItems='center'sm>
+            <Grid container spacing={5} direction='column' justify='center' alignItems='center'sm>
                 <Grid className={classes.title} item container direction="row" justify='center'><Typography variant='h2'>Leaving</Typography></Grid>
-                <Grid container item sm className={classes.formContainer} direction="row" alignItems='center' justify='center'>
+                <Grid container  item sm className={classes.formContainer} alignItems='center' justify='center'>
                         <form noValidate className={classes.form} onSubmit={this.handleSubmit} >
-                            <Typography className={classes.formElements} variant='subtitle1'>When are you leaving?</Typography>
+                            <Grid container item justify='center' alignItems='center'>
+                            <Typography variant='h6'>When are you leaving?</Typography>
                             <KeyboardTimePicker
                             margin="normal"
                             id="time-picker"
@@ -95,19 +144,32 @@ class leave extends Component {
                             KeyboardButtonProps={{
                                 'aria-label': 'change time',
                             }} className={classes.formElements}
-                            fullWidth
                             />
-                            <br />
-                            <TextField error={errors.flexibility ? true : false} helperText={errors.flexibility} id='text' name='flexibility' type='text' value ={this.state.flexibility} className={classes.formElements} label= 'Flexibility' onChange={this.handleChange} fullWidth/>
-                            <br />
-                            <Button variant="contained" type = 'submit' disabled = {this.state.loading} className={classes.formElements}>
+                            <Grid container item justify='center' className={classes.formElements} >
+                                <Typography className={classes.mapElement} variant='h6' >Whats your flexibility?</Typography>
+                                <Slider
+                                    defaultValue={5}
+                                    getAriaValueText={this.valueText}
+                                    valueLabelDisplay="auto"
+                                    step={5}
+                                    marks
+                                    min={0}
+                                    max={15}
+                                />
+                            </Grid>
+                                <Typography className={classes.mapElement} variant='h6'>Where are you parked?</Typography>
+                                <ReactMapGL className='map' mapStyle="mapbox://styles/jerikjakobsen/ck5ezw40x06vp1imw51q8dw8t" {...this.state.viewport} mapboxApiAccessToken={mapboxToken} onViewportChange={viewport => this.setState({viewport})}> 
+                                    <Marker latitude={this.state.viewport.latitude} longitude={this.state.viewport.longitude}>
+                                        <img className = 'mapBoxMarker' src = 'mapBoxMarker.jpg' alt = 'Marker'/>
+                                    </Marker>
+                                </ReactMapGL>
+                            <Button variant="contained" type = 'submit' disabled = {this.state.loading} className={classes.formButton}>
                                 Submit
                             </Button>
+                            </Grid>
                         </form>
                 </Grid>
-                <Grid container item direction="row" justify='center'>
-                        <ReactMapGL className='map' mapStyle="mapbox://styles/jerikjakobsen/ck5ezw40x06vp1imw51q8dw8t" {...this.state.viewport} mapboxApiAccessToken={mapboxToken} onViewportChange={viewport => this.setState({viewport})} />
-                </Grid>
+
                 
             </Grid>
 
